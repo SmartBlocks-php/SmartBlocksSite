0a0133df4a604b2e60cf91c83ee603ef03ab9970
Antoine Jackson <a.j.william26@gmail.com>
2013-11-27 00:24:06 +0100

    + Added header and auto header insertion

--- Public/main.js
+++ Public/main.js
@@ -3,15 +3,47 @@ define([
     'underscore',
     'backbone',
     './apps/landing_page/views/landing_page',
+    'text!./resources/templates/header_template.html',
     './resources/dist/js/bootstrap.min'
-], function ($, _, Backbone, LandingPageView) {
+], function ($, _, Backbone, LandingPageView, header_tpl) {
+
+    //private methods
+    var reset_header_selected = function () {
+        $('.top_menu_bt').removeClass("active");
+        if (SmartBlocks.Url.appname == "BlocksExplorer") {
+            $('.blocks_explorer_tb').addClass('active');
+        } else if (SmartBlocks.Url.appname == "Tutorials") {
+            $('.tutorials_tb').addClass('active');
+        } else if (SmartBlocks.Url.appname == "Documentation") {
+            $('.documentation_mb').addClass('pure-menu-selected');
+        } else if (SmartBlocks.Url.appname == "LandingPage") {
+            $('.documentation_mb').addClass('pure-menu-selected');
+        }
+    }
+
     var main = {
         init: function () {
+            var header_template = _.template(header_tpl, {});
+            $("#top_content").html(header_template);
+
+            reset_header_selected();
+
+            SmartBlocks.events.on("url_changed", function () {
+                reset_header_selected();
+            });
+
 
             $("body").delegate('.left_menu_shower', 'click', function () {
                 SmartBlocks.Blocks.AppDashboard.Main.toggleMenu();
             });
+
+            $("body").delegate(".show_menu_button", "click", function () {
+                if (SmartBlocks.Blocks.AppDashboard) {
+                    SmartBlocks.Blocks.AppDashboard.Main.toggleMenu();
+                }
+            });
         },
+
         launch_landing_page: function () {
 
             var landing_page = new LandingPageView();
